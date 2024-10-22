8b7cd023c8f522d780b3cb2e96deecf793f40ec3
Raman-ji <uniqueraman070@gmail.com>
2024-10-22 17:04:42 +0530

    Config Updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.first_or_create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
